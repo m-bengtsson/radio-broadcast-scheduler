fce7603e35dd3807dcd5028f74bc4d4d46c4b000
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2025-09-10 14:06:44 +0200

    Add swagger
    UI for http requests

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// builder.Services.AddSwaggerUi();
 var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+   app.MapOpenApi();
+   app.UseSwagger();
+   app.UseSwaggerUI();
+}
 
 var schedule = new Schedule();
 schedule.CreateSchedule();
 
 app.MapGet("/api/schedule", () => schedule.WeeklySchedule);
+
 
 app.Run();
